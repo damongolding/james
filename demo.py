c667f76adf38cd0ea42cd0f3f9ae39079a89d26e
Damon <damon@damongolding.com>
2023-08-22 11:15:21 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -89,9 +89,9 @@ class OfficeMonitor:
                     start_time=settings.get("startTime", 7),
                     end_time=settings.get("endTime", 18),
                 )
-                print(self.settings)
+                s(self.settings)
         except Exception as error:
-            print("ERROR", error)
+            self.logger.error("ERROR", error)
             self.settings = Settings(
                 use_celsius=True,
                 on_continually=True,
